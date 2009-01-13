5392313f909307716f6e4dc3c068f7fcb926b5c6
Douglas Creager <dcreager@alum.mit.edu>
2009-01-13 15:06:12 -0800

    Gracefully handle when color-theme isn't installed

--- share/emacs/activate-zenburn.el
+++ share/emacs/activate-zenburn.el
@@ -1,2 +1,7 @@
-(require 'zenburn)
-(zenburn)
+;;; Only try to load zenburn if color-theme is installed.  We don't
+;;; have to check for the existence of zenburn, since we include it in
+;;; ~/share/emacs/lib.
+
+(if (require 'color-theme nil :t)
+    (progn (require 'zenburn)
+           (zenburn)))
