f5ed52b98a4e6f95f1b100b8f5081b04de80cec2
Douglas Creager <dcreager@alum.mit.edu>
2009-05-26 16:31:33 -0400

    Better Emacs fonts on Linux
    This requires the Xft-enabled Emacs, and the Inconsolata font, which can be
    installed on Ubuntu Jaunty using:
    
      sudo apt-get install emacs-snapshot ttf-inconsolata

--- share/emacs/customize/Linux.el
+++ share/emacs/customize/Linux.el
@@ -3,4 +3,11 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 91 :width normal :family "DejaVu Sans Mono")))))
+ '(default ((t (:stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :family "Inconsolata")))))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default-frame-alist (quote ((menu-bar-lines . 1) (background-color . "#3f3f3f") (background-mode . dark) (border-color . "#3f3f3f") (foreground-color . "#dcdccc") (mouse-color . "#dcdccc") (width . 85) (height . 50))))
+ '(inhibit-startup-screen t))
---
+++ share/emacs/os/Linux/post/99-initial-window-size.el
@@ -0,0 +1,1 @@
+(set-frame-size (selected-frame) 85 50)
