3989b3227664e46c5fee4dd2fc97e92c0e603fb5
Douglas Creager <dcreager@alum.mit.edu>
2009-06-02 10:14:48 -0400

    Adding scons rules to auto-mode-alist

--- share/emacs/python-mode.el
+++ share/emacs/python-mode.el
@@ -1,4 +1,4 @@
-(setq auto-mode-alist (cons '("\\.pxd\\'" . python-mode)
-                      (cons
-                            '("\\.pyx\\'" . python-mode)
-                            auto-mode-alist)))
+(add-to-list 'auto-mode-alist '("\\.pxd\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.pyx\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("SConstruct\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("SConscript\\'" . python-mode))
