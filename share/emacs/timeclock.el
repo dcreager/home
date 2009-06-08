87988feabf0694b941ea83a395cd1df28c2e1b57
Douglas Creager <dcreager@alum.mit.edu>
2009-06-08 09:41:44 -0400

    Timeclock mode for Emacs (from ledger source)

---
+++ share/emacs/timeclock.el
@@ -0,0 +1,8 @@
+(require 'timeclock)
+(define-key ctl-x-map "ti" 'timeclock-in)
+(define-key ctl-x-map "to" 'timeclock-out)
+(define-key ctl-x-map "tc" 'timeclock-change)
+(define-key ctl-x-map "tr" 'timeclock-reread-log)
+(define-key ctl-x-map "tu" 'timeclock-update-modeline)
+(define-key ctl-x-map "tw" 'timeclock-when-to-leave-string)
+(add-hook 'kill-emacs-query-functions 'timeclock-query-out)
