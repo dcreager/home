3af88ea6df102f4f2ecc78db20305d5a97bba2fd
Douglas Creager <dcreager@alum.mit.edu>
2009-06-25 10:21:05 -0400

    Autoload protobuf-mode so it works with Emacs 23
    The protobuf-mode file doesn't load correctly on Emacs startup with
    Emacs 23.  You get the following error:
    
      error: `c-lang-defconst` must be used in a file
    
    You can get around this error by autoloading the Emacs mode when it's
    first used.

--- share/emacs/protobuf-mode.el
+++ share/emacs/protobuf-mode.el
@@ -1,2 +1,3 @@
-(require 'protobuf-mode)
+(autoload 'protobuf-mode "protobuf-mode"
+  "Major mode for editing Protocol Buffer files." t)
 (add-to-list 'auto-mode-alist '("\\.proto\\'" . protobuf-mode))
